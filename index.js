b7e927a55290332e758c6142e3d2798bc406293b
rdgzpasserini <m.rodriguezpasserini@gmail.com>
2025-07-31 21:20:41 +0200

    El Jinete Loco

--- index.js
+++ index.js
@@ -9,9 +9,12 @@ app.use(express.static('public'));
 app.get('/', (req, res) => {
   res.send(
 
-    `<h1>Cafè Palmanova</h1>
-    <p>Esment Alimentació</p>
-    <img src="/img.jpg" alt="Cafè Palmanova" width="300">`
+    `<h1>Cuento del Jinete Loco</h1>
+    <h3>Una obra de una tal Sofía.</h3>
+    <br><br>
+    <p>Hace mucho tiempo, en un pueblo pequeño, vivía un jinete que siempre montaba su caballo a toda velocidad. Todos lo llamaban el Jinete Loco, pero nadie sabía por qué corría tanto. Esta es la historia de sus divertidas y misteriosas aventuras.</p>
+    <br>
+    <img src="/img01.jpg" alt="El Jinete Loco" width="1200">`
 
   );
 });
