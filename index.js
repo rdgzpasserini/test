f2a051189330d14873c7ad434fecfd748b44c331
rdgzpasserini <m.rodriguezpasserini@gmail.com>
2025-07-31 10:28:50 +0200

    Configuración para Render con Express

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+// index.js
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('¡Hola Mundo desde Node.js en Render!');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en el puerto ${PORT}`);
+});
+
 console.log('¡Hola Mundo desde Node.js!');
