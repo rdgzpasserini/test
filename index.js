7a21461b1dbeb539dec4a70d27e6bc3a8d4426d4
rdgzpasserini <m.rodriguezpasserini@gmail.com>
2025-07-31 11:07:26 +0200

    Modificado texto 2

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('¡Hola Mundo desde Node.js en Render!');
+  res.send('Hola, muchachos estoy probando el despliegue de mi código en Render. Creo que ya estoy listo para crear seriamente mi portfolio como backend');
 });
 
 const PORT = process.env.PORT || 3000;
