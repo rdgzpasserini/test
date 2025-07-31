ed61e931df7bc0fc1e402168280a28dab72ca5cb
rdgzpasserini <m.rodriguezpasserini@gmail.com>
2025-07-31 16:59:52 +0200

    Modificacion texto img

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-// index.js
+// index.jsc
 const express = require('express');
+const path = require('path');
 const app = express();
 
+// Servir archivos estáticos desde la carpeta 'public'
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-  res.send('Hola, muchachos estoy probando el despliegue de mi código en Render. Creo que ya estoy listo para crear seriamente mi portfolio como backend');
+  res.send(
+
+    `<h1>Cafè Palmanova</h1>
+    <p>Esment Alimentació</p>
+    <img src="/img.jpg" alt="Cafè Palmanova" width="300">`
+
+  );
 });
 
 const PORT = process.env.PORT || 3000;
@@ -11,4 +21,3 @@ app.listen(PORT, () => {
   console.log(`Servidor escuchando en el puerto ${PORT}`);
 });
 
-console.log('Hola, muchachos estoy probando el despliegue de mi código en Render. Creo que ya estoy listo para crear seriamente mi portfolio como backend');
